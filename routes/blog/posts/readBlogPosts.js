930db347c18747a254546371ef238e60f55aecdb
Anders Ravn <anders@adriansen.dk>
2022-07-04 16:38:06 +0200

    Add: Added get route for blogposts

--- routes/blog/posts/readBlogPosts.js
+++ routes/blog/posts/readBlogPosts.js
@@ -1,4 +1,26 @@
+const { status, compose } = require("../../../utilities/composer.js");
+const { MongoClient } = require("mongodb");
+const url = process.env.MONGO_STRING;
+const client = new MongoClient(url);
 
 module.exports = async (req, res) => {
+  try {
+    // Connect to database
+    await client.connect();
+    const db = client.db("singlepage");
+    
+    // Read query
+    const query = await db.collection("posts").find().toArray();
 
-}
+    // If no data is found, return read failed
+    if (!query.length) {
+      return res.json(compose(status.READ.FAILED));
+    }
+
+    // Return query to user
+    return res.json(compose(status.READ.SUCCESS, query));
+  } catch (error) {
+    console.log(error);
+    return res.json(compose(status.READ.FAILED));
+  }
+};
