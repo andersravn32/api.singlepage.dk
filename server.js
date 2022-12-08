dc68904d72f9b33b8d41bc3fe8b1363a1cad0a05
Anders Ravn <anders@adriansen.dk>
2022-12-08 10:19:44 +0100

    Replace: Replaced backend data

--- server.js
+++ server.js
@@ -17,32 +17,57 @@ app.use(
 
 // Enable express body parser
 app.use(
-    express.urlencoded({
-      extended: false,
-    })
-  );
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(express.json());
-
-// Top level app router
-app.use("/", require("./router"));
 
 // Use fallback route
 app.get("*", (req, res) => {
-    res.json({
-        time: Math.round(Date.now() / 1000),
-        hostname: require("./package.json").name,
-        version: require("./package.json").version,
-        description: require("./package.json").description,
-    })
+  res.json({
+    artists: [
+      {
+        name: "Ude af Kontrol",
+        identifier: "ude-af-kontrol",
+        some: {
+          instagram: "",
+          facebook: "",
+          website: "",
+        },
+        image: "/src/assets/img/ude-af-kontrol_cut.png",
+        header:
+          "https://www.information.dk/sites/information.dk/files/styles/930x/public/media/2018/03/23/we_uak1035.jpg?itok=1DEX5cpG",
+        body: "<hello>",
+      },
+      {
+        name: "FABRÄK",
+        identifier: "fabrak",
+        some: {
+          instagram: "",
+          facebook: "",
+          website: "",
+        },
+        image:
+          "https://static.wixstatic.com/media/666a92_fde60b94ef264f558ea60f6a341ea133~mv2.jpg",
+        header:
+          "https://static.wixstatic.com/media/666a92_fde60b94ef264f558ea60f6a341ea133~mv2.jpg",
+        body: "<hello>",
+      },
+    ],
+    program: {
+      users: {},
+    },
+  });
 });
 
 const port = process.env.PORT || 3000;
 
 // Enable server
 server.listen(port, () => {
-  console.log(`Time: ${Math.round(Date.now() / 1000)}`)
-  console.log(`Package: ${require("./package.json").name}`)
-  console.log(`Version: ${require("./package.json").version}`)
-  console.log(`Description: ${require("./package.json").description}`)
+  console.log(`Time: ${Math.round(Date.now() / 1000)}`);
+  console.log(`Package: ${require("./package.json").name}`);
+  console.log(`Version: ${require("./package.json").version}`);
+  console.log(`Description: ${require("./package.json").description}`);
   console.log(`Running server on port ${port}`);
 });
